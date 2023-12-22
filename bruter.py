fb008f7e6f3c142a8032d8efeee5337a2d28c8eb
sharofiddin <pardayev.sharofiddin@gmail.com>
2023-12-22 08:55:36 +0500

    bruter

---
+++ bruter.py
@@ -0,0 +1,60 @@
+import queue
+import requests
+import threading 
+import sys
+AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
+EXTENSIONS = ['.php', '.bak', '.orig', '.inc']
+TARGET = ""
+THREADS = 50
+WORDLIST = "/home/sharofiddin/Downloads/SVNDigger-master/SVNDigger/all.txt"
+            
+
+def get_words(resume=None):
+
+    def extend_words(word):
+        if '.' in word:
+            words.put(f'/{word}')
+        else:
+            words.put(f'/{word}/')
+        for extension in EXTENSIONS:
+            words.put(f'{word}{extension}')
+    with open(WORDLIST) as wl:
+        raw_words = wl.read()
+    
+    found_resume = False
+    words = queue.Queue()
+    for word in raw_words.split():
+        if resume is not None:
+            if found_resume:
+                extend_words(word)
+            elif word == resume:
+                found_resume = True
+                print(f'Resuming wordlist from {word}')
+        else:
+            extend_words(word)
+    return words
+
+
+def dir_bruter(words):
+    headers = {'User-Agent':AGENT}
+    while not words.empty():
+        url = f'{TARGET}{words.get()}'
+        try:
+            r = requests.get(url, headers=headers)
+        except requests.exceptions.ConnectionError:
+            sys.stderr.write('x')
+            sys.stderr.flush()
+            continue
+
+        if r.status_code == 200:
+            print(f'\nSuccess ({r.status_code}:{url})')
+        elif r.status_code == 404:
+            sys.stderr.write('.')
+        else:
+            print(f'\n{r.status_code}->{url}')
+
+if __name__ == '__main__':
+    words = get_words()
+    TARGET = input('Press enter target:')
+    for _ in range(THREADS):
+      threading.Thread(target=dir_bruter ,args= (words,)).start()
